ddfb1ec0e25c5fee45450282e45025f87e43874c
Antonio Sabino <aslimajr@outlook.com>
2022-07-31 18:32:00 -0300

    commit bomba do meu primeiro projeto em Java

---
+++ src/projetoyoutube/ProjetoYouTube.java
@@ -0,0 +1,35 @@
+package projetoyoutube;
+
+public class ProjetoYouTube {
+    public static void main(String[] args) {
+        Video[] videos = new Video[3];
+        videos[0] = new Video("Aula 1 de POO");
+        videos[1] = new Video("Aula 12 de PHP");
+        videos[2] = new Video("Aula 10 de HTML5");
+
+        Gafanhoto[] gafanhoto = new Gafanhoto[2];
+
+        gafanhoto[0] = new Gafanhoto("Sabino", 29, "M", "sabino");
+        gafanhoto[1] = new Gafanhoto("Junior", 19, "M", "axterix");
+
+        Visualizacao[] vis = new Visualizacao[5];
+        vis[0] = new Visualizacao(gafanhoto[0], videos[1]);
+        vis[0].avaliar(87.0f);
+        vis[1] = new Visualizacao(gafanhoto[0], videos[0]);
+        vis[1].avaliar();
+
+        System.out.println(vis[0]);
+        System.out.println(vis[1]);
+
+
+//        System.out.println("VIDEOS\n-----------------------------");
+//        System.out.println(videos[0]);
+//        System.out.println(videos[1]);
+//        System.out.println(videos[2]);
+//
+//        System.out.println("\nGAFANHOTOS\n-----------------------------");
+//        System.out.println(gafanhoto[0]);
+//        System.out.println(gafanhoto[1]);
+
+    }
+}
